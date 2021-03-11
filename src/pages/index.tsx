7cfbd444c571584156d48a3f6582733cb5341fa8
THiragi <w1th1nmybr41n@gmail.com>
2021-03-11 14:19:45 +0900

    added tone.js and chakra ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,64 +1,31 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+
+import * as Tone from 'tone';
 import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => (
-  <div className={styles.container}>
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home: NextPage = () => {
+  const handleSynth = () => {
+    const osc = new Tone.Oscillator().toDestination();
+    osc.frequency.value = 'C4';
+    osc.frequency.rampTo('C7', 2);
+    osc.start().stop('+3');
+  };
 
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Next + tone</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <p className={styles.description}>
-        Get started by editing{' '}
-        <code className={styles.code}>pages/index.js</code>
-      </p>
-
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className={styles.card}>
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/vercel/next.js/tree/master/examples"
-          className={styles.card}
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className={styles.card}
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-        </a>
-      </div>
-    </main>
-
-    <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-      </a>
-    </footer>
-  </div>
-);
+      <main className={styles.main}>
+        <button type="button" onClick={handleSynth}>
+          play synth
+        </button>
+      </main>
+    </div>
+  );
+};
 
 export default Home;
